97f9b0891185cca29121baf3e01c855cdd56768a
Denis <Ori21wiki@gmail.com>
2026-04-07 11:49:21 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <section className="workspace">
+        <h1>Roulette</h1>
+        <p>База очищена. Можно собирать экран и анимацию.</p>
+      </section>
+    </main>
+  )
+}
+
+export default App
